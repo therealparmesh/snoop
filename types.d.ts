42c24b522932581e733dce95a8d9e5b71f70fad8
Parmesh Krishen <therealparmesh@outlook.com>
2026-07-11 18:19:02 -0500

    fix: correct spy declarations

--- types.d.ts
+++ types.d.ts
@@ -1,27 +1,30 @@
-export interface Fn {
-  (...args: any): any;
-}
+export type Fn = (...args: any[]) => any;
 
 export interface Call<T extends Fn> {
   arguments: Parameters<T>;
-  result: ReturnType<T>;
-  error: Error;
+  result: ReturnType<T> | undefined;
+  error: unknown;
+}
+
+export interface SnoopReference {
+  readonly testID: number;
+  readonly notCalled: boolean;
 }
 
 export interface Snoop<T extends Fn> {
   fn: T;
-  testID: number;
-  called: boolean;
-  notCalled: boolean;
-  calledOnce: boolean;
-  callCount: number;
-  calls: Call<T>[];
-  firstCall: Call<T>;
-  lastCall: Call<T>;
-  calledBefore: (other: Snoop) => boolean;
-  calledAfter: (other: Snoop) => boolean;
-  calledImmediatelyBefore: (other: Snoop) => boolean;
-  calledImmediatelyAfter: (other: Snoop) => boolean;
+  readonly testID: number;
+  readonly called: boolean;
+  readonly notCalled: boolean;
+  readonly calledOnce: boolean;
+  readonly callCount: number;
+  readonly calls: Call<T>[];
+  readonly firstCall: Call<T> | undefined;
+  readonly lastCall: Call<T> | undefined;
+  calledBefore: (other: SnoopReference) => boolean;
+  calledAfter: (other: SnoopReference) => boolean;
+  calledImmediatelyBefore: (other: SnoopReference) => boolean;
+  calledImmediatelyAfter: (other: SnoopReference) => boolean;
 }
 
 export function snoop<T extends Fn>(fn: T): Snoop<T>;
